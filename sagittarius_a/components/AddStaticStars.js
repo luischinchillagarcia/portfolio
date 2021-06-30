fe86098ed2c78954706ec2277b509811346b61b1
lchinchillagarcia <luis.chinchilla-garcia@redbull.com>
2021-06-30 09:56:52 -0700

    refactor: add base template for static stars

--- sagittarius_a/components/AddStaticStars.js
+++ sagittarius_a/components/AddStaticStars.js
@@ -3,29 +3,42 @@ import StaticStar from './StaticStar'
 
 import { motion } from "framer-motion"
 
-function AnimationWrapper({ children, duration=2, delay=0 }) {
+function AnimationWrapper({ children, duration=2, delay=0, useMotion=false }) {
+
+  const transitionConfig = {
+    duration,
+    delay,
+    direction: 'alternate',
+    ease: "linear",
+    times: [0, .8, 1],
+    repeat: Infinity,
+    repeatDelay: 1,
+  }
+
+  let wrapper = null
+  if (useMotion) {
+    wrapper = (
+      <motion.div 
+        animate={{ opacity: [0, 1, 0] }}
+        transition={transitionConfig}
+      >
+        { children }
+      </motion.div>
+    )
+  }
+  else {
+    wrapper = <>{ children }</>
+  }
+  
   return (
-    <motion.div 
-      animate={{ 
-        opacity: [0, 1, 0],
-      }}
-      transition={{
-        duration,
-        delay,
-        direction: 'alternate',
-        ease: "linear",
-        times: [0, .8, 1],
-        repeat: Infinity,
-        repeatDelay: 1,
-      }}
-    >
-      { children }
-    </motion.div>
+    <>
+      { wrapper }
+    </>
   )
 }
 
 
-function AddStaticStars({ numStars, gapLo=45, gapHi=55, size=20, sizeVar=10, opacity=0.6, opacityVar=0.4, duration=3.5, durationVar=1.5, classes='' }) {
+function AddStaticStars({ numStars, gapLo=45, gapHi=55, size=20, sizeVar=10, opacity=0.6, opacityVar=0.4, duration=3.5, durationVar=1.5, classes='', useMotion=false }) {
 
   const [configs, setConfigs] = useState([])
 
@@ -33,7 +46,6 @@ function AddStaticStars({ numStars, gapLo=45, gapHi=55, size=20, sizeVar=10, opa
   const randNum = (lo, hi) => {
     let lowerRange = randNumRange(0, lo)
     let upperRange = randNumRange(hi, 100)
-
     let randRange = (Math.random() > 0.5) ? 1 : 0
     let chooseRandRange = (randRange === 0) ? lowerRange : upperRange
 
@@ -49,24 +61,25 @@ function AddStaticStars({ numStars, gapLo=45, gapHi=55, size=20, sizeVar=10, opa
   })
 
   useEffect(() => {
-    let confs = []
+    let starConfigs = []
     for (let s = 0; s < numStars; s++) {
       let {left, top, size, opacity, duration} = xyPositions(gapLo, gapHi)
-      confs.push({left, top, size, opacity, duration})
+      starConfigs.push({left, top, size, opacity, duration})
     }
-    setConfigs(confs)
+    setConfigs(starConfigs)
   }, []);
 
-  const allClasses = 'absolute stars-twinkle ' + classes
+  let allClasses = classes + ' absolute ' //+ ' animate-pulse '// + ' stars-twinkle '
+  allClasses += useMotion ? '' : ' stars-twinkle '
   const wrapperStyles = (config) => ({duration: config.duration, delay: config.delay})
 
   return (
     <>
       { 
         configs.map((conf, index) => (
-          <AnimationWrapper key={index} {...wrapperStyles(conf)} >
-            <StaticStar 
-              cssUnit="px" classes={allClasses} size={conf.size} left={conf.left} top={conf.top} opacity={conf.opacity}
+          <AnimationWrapper key={'staticStar-' + index} {...wrapperStyles(conf)} useMotion={useMotion} >
+            <StaticStar
+              cssUnit="px" classes={allClasses + ` anim-delay-${index % 3}`} size={conf.size} left={conf.left} top={conf.top} opacity={conf.opacity}
               style={{ animationDuration: conf.duration }}
             />
           </AnimationWrapper>
